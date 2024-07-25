fa4cb2b77c2516bc5951bdbbf446bd851bc71d23
User <fadhillatmojo@gmail.com>
2024-07-26 02:14:15 +0700

    opting out of caching

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,8 @@
+import { cookies } from "next/headers";
+
+
+export const fetchCache = "default-cache";
+
 type Product = {
 	"id": number;
 	"title": string;
@@ -8,8 +13,14 @@ type Product = {
 
 export default async function ProductsPage() {
 	const response = await fetch("http://localhost:3001/products");
-	// in next 14 app, without reload, the data that already fetched are saved in the cache folder, so it will not request on the api server
 	const products = await response.json();
+
+	const cookieStore = cookies();
+	cookieStore.get("theme");
+
+	const detailResponse = await fetch("http://localhost:3001/products/1");
+	const details = await detailResponse.json();
+	// in next 14 app, without reload, the data that already fetched are saved in the cache folder, so it will not request on the api server
 	return (
 		<div className="grid grid-cols-2 gap-2 p-4">
 			{products.map((product: Product) => (
@@ -20,6 +31,7 @@ export default async function ProductsPage() {
 					</div>
 					<div className="flex flex-col space-y-1 items-end">
 						<h2 className="text-md">{product.description}</h2>
+						<h2 className="text-md">{details.price}</h2>
 					</div>
 				</div>
 			))}
