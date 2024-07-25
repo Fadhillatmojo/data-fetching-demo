4e620b581c779167a036ef9de319a323404f5273
User <fadhillatmojo@gmail.com>
2024-07-25 18:31:03 +0700

    fetch data with node server and check caching

---
+++ src/app/products/page.tsx
@@ -0,0 +1,28 @@
+type Product = {
+	"id": number;
+	"title": string;
+	"price": number;
+	"description": string;
+};
+
+
+export default async function ProductsPage() {
+	const response = await fetch("http://localhost:3001/products");
+	const products = await response.json();
+	return (
+		<div className="grid grid-cols-2 gap-2 p-4">
+			{products.map((product: Product) => (
+				<div key={product.id} className="flex items-center justify-between p-4 bg-white shadow rounded-lg text-black">
+					<div className="flex flex-col space-y-1">
+						<h2 className="text-lg font-semibold">{product.title}</h2>
+						<h2 className="text-sm">Price: $ {product.price}</h2>
+					</div>
+					<div className="flex flex-col space-y-1 items-end">
+						<h2 className="text-md">{product.description}</h2>
+					</div>
+				</div>
+			))}
+
+		</div>
+	);
+}
