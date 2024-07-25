fb1de9fe7527234584879ff56d1650abc70acebc
User <fadhillatmojo@gmail.com>
2024-07-26 02:25:38 +0700

    request memoization

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,5 @@
-import { cookies } from "next/headers";
-
-
-export const fetchCache = "default-cache";
+// import { cookies } from "next/headers";
+// export const fetchCache = "default-cache";
 
 type Product = {
 	"id": number;
@@ -15,11 +13,11 @@ export default async function ProductsPage() {
 	const response = await fetch("http://localhost:3001/products");
 	const products = await response.json();
 
-	const cookieStore = cookies();
-	cookieStore.get("theme");
+	// const cookieStore = cookies();
+	// cookieStore.get("theme");
 
-	const detailResponse = await fetch("http://localhost:3001/products/1");
-	const details = await detailResponse.json();
+	// const detailResponse = await fetch("http://localhost:3001/products/1");
+	// const details = await detailResponse.json();
 	// in next 14 app, without reload, the data that already fetched are saved in the cache folder, so it will not request on the api server
 	return (
 		<div className="grid grid-cols-2 gap-2 p-4">
@@ -31,7 +29,7 @@ export default async function ProductsPage() {
 					</div>
 					<div className="flex flex-col space-y-1 items-end">
 						<h2 className="text-md">{product.description}</h2>
-						<h2 className="text-md">{details.price}</h2>
+						{/* <h2 className="text-md">{details.price}</h2> */}
 					</div>
 				</div>
 			))}
